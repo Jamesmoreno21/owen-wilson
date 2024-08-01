226af9d080e115970344f816f7671ba05771c6f7
Jamesmoreno21 <jaime.moreno@uc.cl>
2024-07-31 22:10:33 -0400

    fix: use timestamp to sort on same movie wows

--- src/app/extra-info/page.tsx
+++ src/app/extra-info/page.tsx
@@ -23,10 +23,15 @@ const sortWowsByDuration = (wows: Wow[]) => {
 
 const sortWowsByReleaseDateAndCurrentWow = (wows: Wow[]) => {
   return wows.sort((a, b) => {
-    if (new Date(a.release_date).getTime() === new Date(b.release_date).getTime()) {
-      return a.current_wow_in_movie - b.current_wow_in_movie;
+    if (a.movie === b.movie) {
+      return (
+        parseDurationToSeconds(a.timestamp) -
+        parseDurationToSeconds(b.timestamp)
+      );
     }
-    return new Date(a.release_date).getTime() - new Date(b.release_date).getTime();
+    return (
+      new Date(a.release_date).getTime() - new Date(b.release_date).getTime()
+    );
   });
 };
 
@@ -45,12 +50,12 @@ const getMedianWows = (wows: Wow[]) => {
 const getFirstAndLastWows = (wows: Wow[]) => {
   const sortedWows = sortWowsByReleaseDateAndCurrentWow(wows);
   return [sortedWows[0], sortedWows[sortedWows.length - 1]];
-}
+};
 
 const getLongerMovieWow = (wows: Wow[]) => {
   const sortedWows = sortWowsByDuration(wows);
   return sortedWows[sortedWows.length - 1];
-}
+};
 
 export default async function ExtraInfoPage() {
   const wows = await getAllWows();
@@ -63,7 +68,10 @@ export default async function ExtraInfoPage() {
       <h1 className="text-4xl font-bold">Extra Information</h1>
       <div className="flex flex-col items-center justify-center gap-4 w-full my-4">
         <WowsVisualizer wows={[longerMovieWow]} title="Longest Movie" />
-        <WowsVisualizer wows={[firstWow, lastWow]} title="First and Last Wow respectively" />
+        <WowsVisualizer
+          wows={[firstWow, lastWow]}
+          title="First and Last Wow respectively"
+        />
         <WowsVisualizer wows={medianWows} title="Median Wows" />
       </div>
     </div>
